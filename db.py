77c6d04e43b26339ec9dfd04ea0fc9102fd85bf7
swoody2527 <104504405+swoody2527@users.noreply.github.com>
2025-08-21 14:30:58 +0100

    initial setup

---
+++ db.py
@@ -0,0 +1,11 @@
+from sqlmodel import create_engine, Session
+
+sqlite_url = f'sqlite:///dev.db'
+
+engine = create_engine(
+    sqlite_url, connect_args={'check_same_thread': False}
+    )
+
+def get_session():
+    with Session(engine) as session:
+        yield session
